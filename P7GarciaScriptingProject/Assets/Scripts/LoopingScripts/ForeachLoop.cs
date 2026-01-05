87df3b4510be5d2bf29e1557ae63b82eeb5cd529
AwesomeSauce-67 <750433@student.dusd.net>
2026-01-05 14:08:06 -0800

    Graded Assignments

---
+++ P7GarciaScriptingProject/Assets/Scripts/LoopingScripts/ForeachLoop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForeachLoop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] strings=new string[3];
+
+        strings[0] = "First String";
+        strings[1] = "Second String";
+        strings[2] = "Third String";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
